21139711be197b57e2ac98417e5ac97796b71c2b
Tim Lehane <timlehane1998@gmail.com>
2021-11-10 18:27:04 +0000

    SQL_SCRIPTS now contains a fully functioning CREATE TABLE statement as well as a Data isnertion script that populates the table. Database is live and up and running. Now time to move to the API to pull the DATA using SELECT *

--- SQL_SCRIPTS/sql_script_01.sql
+++ SQL_SCRIPTS/sql_script_01.sql
@@ -1,26 +1,26 @@
 /* Lines Table */
 
-DROP TABLE IF EXISTS Lines;
+DROP TABLE IF EXISTS LinesTable;
 
-CREATE TABLE Lines (
-    fId int NOT NULL AUTO_INCREMENT,
-    objectId int NOT NULL,
+CREATE TABLE LinesTable (
+    fId INT NOT NULL,
+    objectId INT NOT NULL,
     lineName varchar(127) NOT NULL,
-    easting int(6) NOT NULL,
-    northing int(6) NOT NULL,
-    lines varchar(255) NOT NULL,
+    easting INT(6) NOT NULL,
+    northing INT(6) NOT NULL,
+    `lines` varchar(255) NOT NULL,
     network varchar(127) NOT NULL,
     lineZone INT NOT NULL,
     x FLOAT(10, 8) NOT NULL,
     y FLOAT(10, 8) NOT NULL,
-    PRIMARY KEY (Id)
+    PRIMARY KEY (fId)
 );
 
 /* https://dev.mysql.com/doc/refman/8.0/en/keywords.html#keywords-new-8-0-Z */
 
-INSERT INTO Users (FirstName, LastName, Username, Password) VALUES ("Tim", "Lehane", "timlehane");
+INSERT INTO Lines (FirstName, LastName, Username, Password) VALUES ("Tim", "Lehane", "timlehane");
 
-SELECT * FROM Users;
+SELECT * FROM Lines;
 
 /* Stations Table */
 
